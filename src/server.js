1d25507444828de830e4c7faa735017eb7309f84
Marius <mariushaf@gmail.com>
2022-08-26 12:01:43 +0300

    server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+const mysql = require('mysql2/promise')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,24 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.json({ msg: 'Hello World' });
 });
+app.get('/api/posts', async (req , res)=> {
+ try {
+     // 1. prisijungti prie db
+  const connection = await mysql.createConnection(dbConfig)
+  console.log('Connected to db'.bgGreen.bold)
+ // 2. atlikti veiksma
+  const sql = 'SELECT * FROM posts';
+  const [rows, fields] = await connection.query(sql)
+  console.log('rows ---->', rows);
+  res.json(rows)
+ // 3.uzdaryti prisijungima
+ connection.end()
+ } catch (error) {
+     console.log('Error Connecting to db'.bgRed.bold, error)
+     // 4. gaudyti klaidas
+     res.status(500).json({ msg: 'something went wrong' })
+ }
+})
 
 app.use((req,res) => {
   res.status(404).json({
